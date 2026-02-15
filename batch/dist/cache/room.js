5be032f87d4af0e192ac2f5e8c9114fa58bc7fab
Varunjoshi-19 <varunjoshi6283@gmail.com>
2026-02-15 16:38:17 +0530

    saved

--- batch/dist/cache/room.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.trackUsersRoom = exports.createdRooms = void 0;
-const createdRooms = new Map();
-exports.createdRooms = createdRooms;
-const trackUsersRoom = new Map();
-exports.trackUsersRoom = trackUsersRoom;
