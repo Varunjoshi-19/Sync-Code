7ff7b5b8a16cf5cc7ac971435a92b9d180e5bae4
Varunjoshi-19 <varunjoshi6283@gmail.com>
2026-02-15 14:53:50 +0530

    fixed prisma config file!

--- batch/prisma.config.ts
+++ batch/prisma.config.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv"
 import { defineConfig } from "prisma/config";
-
-dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -12,3 +9,4 @@ export default defineConfig({
     url: process.env["DATABASE_URL"],
   },
 });
+
